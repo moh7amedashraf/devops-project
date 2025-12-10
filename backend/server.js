66de5d7cefa87871b9916aa37785e9a5cc5d1d87
moh7amedashraf <mohaelshabah@gmail.com>
2025-12-10 16:58:46 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to the API root!" });
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the backend API!" });
 });
